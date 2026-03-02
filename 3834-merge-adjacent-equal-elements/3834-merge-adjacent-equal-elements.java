cea2edfab3ef01ce0ae75bfee3fbf94d447ee5e4
SOURABHKUMAR-02 <kumarsourabh.m@gmail.com>
2026-03-02 19:21:57 +0530

    Time: 102 ms (20.31%), Space: 124.8 MB (87.25%) - LeetHub

---
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Long> mergeAdjacent(int[] nums) {
+        Stack<Long> stack = new Stack<>();
+        stack.push((long) nums[0]);
+        for (int i =1; i <nums.length; i++) {
+            long a=(long)nums[i];
+            if (!stack.isEmpty() && a== stack.peek()) {
+                a=2*stack.pop();
+                while(!stack.isEmpty() && a== stack.peek()) {
+                    a=2*stack.pop();
+               
+            } 
+             stack.push((long) a);
+            } else {
+                stack.push((long) nums[i]);
+            }
+        }
+        List<Long> l = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            l.add(stack.pop());
+        }
+
+       Collections.reverse(l);
+        return l;
+    }
+}
